039000af396561a1b63a878e4289c858f721cde1
xavieryuan <xavieryuan@gmail.com>
2014-08-26 14:52:02 +0800

    JS crash course added

--- main.js
+++ main.js
@@ -1,133 +1,128 @@
-//定义全局变量
-var queue;//资源加载器
-var stage; //舞台
-var bg; //背景
-var hero; //玩家控制的角色
-var rockList=[];//陨石数组
-var sparkList=[];//火花数组
-var move;
-var lockHero=false;
-var gameState="STARTED";//"STARTED","PAUSED"
+//-----注释
+/*	
+	双斜杠用于注释单行代码。
+	如果需要注释的代码有许多行,
+	最好使用斜杠和星号添加首尾注释，
+	注意，首尾注释的样式是对称的。
+	程序运行的时候，代码中被注释的部分不会被执行。
+*/
 
-//初始化，加载资源
-function init() {
-	//全局参数设置
-    createjs.Ticker.setFPS(60);
-	
-    //加载资源
-	//先构建一个资源加载器
-	queue = new createjs.LoadQueue(false);
-	//绑定加载事件处理
-	queue.on("complete", setupStage, this);
-	//开始加载
-	queue.loadFile({id:"hero_asset", src:"images/hero.png"});
-	queue.loadFile({id:"meteor_asset", src:"images/meteor.png"});
-	queue.loadFile({id:"bgB_asset", src:"images/bgB.png"});
-	queue.loadFile({id:"bgC_asset", src:"images/bgC.png"});
-    }
+//-----调试
+/*
+	//控制台日志
+	//console.log() 是很常用的调试工具
+	//可以让特定内容显示在浏览器的控制台，比如
+	console.log("你好！");
+*/
 
-//构建场景
-function setupStage(){
-	//创建舞台
-    stage = new createjs.Stage("gameCanvas");
-	createjs.Touch.enable(stage);
-    
-	//创建背景
-    bg = Background(queue,stage);
-	
-	//创建角色
-	hero = Hero(queue,stage);
-	
-	//生成三个陨石
-	generateRock();
-	generateRock();
-	generateRock();
-	
-	
-	//绑定舞台点击事件
-    stage.addEventListener("click",handleStageClick); 
-    //心跳启动
-    createjs.Ticker.addEventListener("tick",tick);	
-	hero.fall()
-}
+//-----数据类型
+/*
+	//程序运行的过程就是一种数据运算的过程
+	//但数据有不同的类型，其运算处理的方式有差异
+	//JavaScript中，经常用到的数据类型包括Number,String,Boolean等等
 
-//函数
-function generateRock(speed){
-	var rock = Rocks(queue.getResult("meteor_asset"),stage,speed);
-	rockList.push(rock);
-}
-function generateSpark(xpos,ypos){
-	var spark = Spark(xpos,ypos,stage)
-	sparkList.push(spark);
-}
+	//---Number,数值型数据，例如 1，20，40
+	//数值型数据可以进行数学计算
+	//例如1+1，40/2，20*8
+	console.log(20*8/3);
 
-//球形算法
-function checkCollision(ballA,ballB){
-	var dxy=(ballA.x-ballB.x)*(ballA.x-ballB.x)+(ballA.y-ballB.y)*(ballA.y-ballB.y)
-	var dr=(ballA.r+ballB.r)*(ballA.r+ballB.r)
-	if(dxy>dr){
-		return false;
-	}else{
-		return true;
+	//---String,字符串型数据
+	//例如 "你好"，"Hello", "1"
+	//字符串型数据不能进行数学计算
+	//但可以进行字符处理，例如
+	console.log("你"+"好");
+
+	//---Boolean,布尔型数据
+	//布尔型数据只有两种值
+	//true 代表 真
+	//false 代表 假
+	//布尔型数据是布尔计算的结果，经常用于代码中的逻辑判断
+	console.log(1<2);
+*/
+
+//-----变量
+/*
+	//在数据处理过程中
+	//变量是用来存放数据的容器
+	//如果把数据处理比喻成烹饪
+	//那么数据就是食物
+	//而变量就是装食物的盘子
+	//例如，下面的代码声明了若干变量，同时给变量赋了不同的值
+	var a=100;
+	var b=2;
+	var c="Hello";
+	var d=true;
+	
+	//当然，也可以分两个步骤来声明和赋值
+	var e;
+	e=99;
+	
+	//还记得console.log()么？可以用它来查看变量这个盒子里装了什么值
+	console.log(e);
+	
+	//变量这个盒子里可以装各种各样的数据，除了数值、字符串等类型的数据外，还可以是函数、图片、视频等等
+	//我们会在今后用到这些类型时介绍具体的用法。
+*/
+
+//-----数组
+/*
+	//数组的作用跟变量很相似，也是存放数据的一种容器。
+	//不同的是，一个数组中可以存放多个数据，并且使用数字编号索引这些数据
+	//声明一个空的数组
+	var skills = [];
+	//现在开始往数组里加东西
+	skills.push("小型枪械");
+	skills.push("开锁");
+	skills.push("修理");
+	skills.push("急救");
+	skills.push("采药");
+	//然后我们可以通过编号索引这些数据
+	console.log(skills[0]);//"小型枪械"
+	console.log(skills[3]);//"急救"
+	//注意，索引是从0开始的
+	//通过skills.length可以获知当前数组里有多少个数据
+	console.log(skills.length);//5
+	
+	//数组除了push指令来添加数据外，还有许多其他的操作，比如删除、合并、排序等等。
+	//我们会在今后用到这些指令的地方介绍其用法。
+*/
+
+//-----条件与判断
+/*
+	//在编写脚本时，经常需要根据布尔运算的结果执行不同的代码，比如
+	var health = 100;
+	var damage = 100;
+	if(health-damage<=0){
+		console.log("英雄挂了！")
+	}else if (health-damage<=20) {
+		console.log("英雄：咔，好痛！")
+	}else if (health-damage<=80){
+		console.log("英雄：还有两下子！")
+	}else {
+		console.log("英雄：你在挠痒痒么！")
 	}
-}
+*/
 
-//事件处理
-function tick(evt){	
-	
-	
-	//陨石的移动和更新
-	var newRockList=[]
-	for (var i = 0;i<rockList.length;i++){
-		rockList[i].update();
-		if(!rockList[i].removable){
-		newRockList.push(rockList[i])
-		}else{
-			stage.removeChild(rockList[i])
-			generateRock()
-		}
+//-----函数
+/*
+	//函数是一组定义了特定功能的程序代码块，以下定义了一个函数 sayHello
+	function sayHello(){
+		console.log("Hello World!");
 	}
-	rockList = newRockList;
-	
-	//玩家撞到屏幕边界
-	if(hero.y<=20){
-		hero.knockOnCeiling()
-		generateSpark(hero.x,hero.y);
-	}else if(hero.y>=520){		
-		hero.knockOnBottom();
-	}	
-	
-	//碰撞检测
-	for (var i = 0;i<rockList.length;i++){
-		if(checkCollision(hero.getHotspot(),rockList[i].getHotspot())){
-			var collidingPointX=rockList[i].x+(hero.x-rockList[i].x)/2;
-			var collidingPointY=rockList[i].y+(hero.y-rockList[i].y)/2;
-			generateSpark(collidingPointX,collidingPointY);			
-			if(hero.y>=rockList[i].y){
-				hero.pushDown()
-			}else{
-				hero.pushUp()
-			}
-		}		
+	//函数定义后可被重复调用，以下代码连续调用了两次sayHello
+	sayHello();
+	sayHello();
+
+	//函数可能包含零个或多个参数，
+	function sayHiTo(who){
+		console.log("Hi "+who+"!");
 	}
-	
-	//火花的清理
-	var newSparkList=[]
-	for (var i = 0;i<sparkList.length;i++){
-		if(!sparkList[i].removable){
-		newSparkList.push(sparkList[i])
-		}else{
-			stage.removeChild(sparkList[i])
-		}
+
+	//函数可能返回一个值
+	function getCircleArea(r){
+		var area= Math.PI*r*r;
+		return area;
 	}
-	sparkList = newSparkList;
-	
-	//循环更新视图
-	bg.update()
-    stage.update();
-}
-function handleStageClick(evt){
-	if(!hero.stunned){
-		hero.jump()
-	}    
-}
+	console.log(getCircleArea(12));
+	//注意，上面代码中的area是函数getCircleArea的一个内部变量，其作用域只在函数getCircleArea内部有效。对于函数定义之外的代码来说，函数的内部就像一个黑盒子，无法直接访问和调用。
+*/
